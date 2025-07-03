f8c71d9c2f87f637da6c45f913ec989b76b74304
LoicZardel <ngouzokuitche@gmail.com>
2025-07-03 16:59:06 +0200

    page 2 ok

--- rect projet/trouvetonplat/src/App.js
+++ rect projet/trouvetonplat/src/App.js
@@ -1,12 +1,14 @@
 import Accueil from './Accueil'
+import Resto from './Resto'
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Accueil/>
+        
       <Routes>
         <Route path='/' element={<Accueil />} />
+        <Route path='/Resto' element={<Resto />} />
       </Routes>
     </div>
   );
